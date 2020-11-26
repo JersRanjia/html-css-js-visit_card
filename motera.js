f19fa61bc539f651967cb69a6a9479e0165fe328
JersRanjia <jersrandria@yahoo.fr>
2020-11-26 09:53:43 +0300

    remplacer tous les animations et mis des images à la place des labels

--- motera.js
+++ motera.js
@@ -1,4 +1,8 @@
 
+const classe_disparaitFace = "disparait-face" ;
+const classe_apparaitFace = "apparait-face" ;
+const classe_disparaitBack = "disparait-back" ;
+
 function tourner() {
 
     var face = document.getElementById("face") ;
@@ -7,22 +11,25 @@ function tourner() {
     var bt_tourner = document.getElementById("tourner") ;
     bt_tourner.disabled = true ;
 
-    if (face.classList.contains("disparait")) {
+    if (face.classList.contains(classe_disparaitFace)) {
 
-        face.classList.remove("disparait")
-        face.classList.add("apparait")
-        face.style.zIndex = 3 ;
+        face.classList.remove(classe_disparaitFace)
+        face.classList.add(classe_apparaitFace)
+        back.classList.add(classe_disparaitBack)
         face.onanimationend = function (e) {
             face.style.zIndex = 3 ;
+            face.style.filter = "blur(0px)"
             bt_tourner.innerText = "<"
         }
 
     } else {
 
-        face.classList.remove("apparait")
-        face.classList.add("disparait")
+        face.classList.remove(classe_apparaitFace)
+        face.classList.add(classe_disparaitFace)
+        back.classList.remove(classe_disparaitBack)
         face.onanimationend = function (e) {
-            face.style.zIndex = 1 ;            bt_tourner.innerText = "<"
+            face.style.zIndex = 1 ;        
+            face.style.filter = "blur(10px)"
             bt_tourner.innerText = ">"
         }
     }
